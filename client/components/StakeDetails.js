47e83391d23c98f82414cbd0668470674f559d85
sumana10 <sumanaonlyme@gmail.com>
2023-03-09 19:10:36 +0530

    first commit

---
+++ client/components/StakeDetails.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useMoralis, useWeb3Contract } from 'react-moralis';
+import StakingAbi from '../constants/Staking.json';
+import TokenAbi from '../constants/RewardToken.json';
+
+function StakeDetails() {
+  const { account, isWeb3Enabled } = useMoralis();
+  const [rtBalance, setRtBalance] = useState('0');
+  const [stakedBalance, setStakedBalance] = useState('0');
+  const [earnedBalance, setEarnedBalance] = useState('0');
+
+  const stakingAddress = "0x30b4b1150BCD5B838812a41BD9d46d61FF0c32c9"; //replace this with the address where you have deployed your staking Smart Contract
+  const rewardTokenAddress = "0x1fa9C2FDdc7C747aeCbf7D4E6dAd280da4D5607b"; //replace this with the address where you have deployed your Reward Token Smart Contract
+
+  const { runContractFunction: getRTBalance } = useWeb3Contract({
+    abi: TokenAbi.abi,
+    contractAddress: rewardTokenAddress,
+    functionName: 'balanceOf',
+    params: {
+      account
+    }
+  });
+
+  const { runContractFunction: getStakedBalance } = useWeb3Contract({
+    abi: StakingAbi.abi,
+    contractAddress: stakingAddress,
+    functionName: 'getStaked',
+    params: {
+      account
+    }
+  });
+
+  const { runContractFunction: getEarnedBalance } = useWeb3Contract({
+    abi: StakingAbi.abi,
+    contractAddress: stakingAddress,
+    functionName: 'earned',
+    params: {
+      account
+    }
+  });
+
+
+  useEffect(() => {
+    async function updateUiValues() {
+      const rtBalance = (await getRTBalance({ onError: (error) => console.log(error) })).toString();
+      const formattedRtBalance = parseFloat(rtBalance) / 1e18;
+      const formattedRtBalaceRounded = formattedRtBalance.toFixed(2);
+      setRtBalance(formattedRtBalaceRounded);
+
+      const stakedBalace = (await getStakedBalance({ onError: (error) => console.log(error) })).toString();
+      const formattedStakedBalance = parseFloat(stakedBalace) / 1e18;
+      const formattedStakedBalanceRounded = formattedStakedBalance.toFixed(2);
+      setStakedBalance(formattedStakedBalanceRounded);
+
+       const earnedBalance = (await getEarnedBalance({ onError: (error) => console.log(error) })).toString();
+       console.log(earnedBalance)
+       const formattedEarnedBalance = parseFloat(earnedBalance) / 1e18;
+       console.log(formattedEarnedBalance)
+       const formattedEarnedBalanceRounded = formattedEarnedBalance.toFixed(2);
+       console.log(formattedEarnedBalanceRounded)
+       setEarnedBalance(earnedBalance);
+    }
+
+    if (isWeb3Enabled) updateUiValues();
+  
+}, [account, getEarnedBalance, getRTBalance, getStakedBalance, isWeb3Enabled]);
+return (
+    <div className='p-3'>
+      <div className='font-bold m-2'>RT Balance is: {rtBalance}</div>
+      <div className='font-bold m-2'>Earned Balance is: {earnedBalance}</div>
+       <div className='font-bold m-2'>Staked Balance is: {stakedBalance}</div> 
+    </div>
+  );
+}
+
+export default StakeDetails;
---
+++ client/components/StakeForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useWeb3Contract } from 'react-moralis';
+import StakingAbi from '../constants/Staking.json';
+import TokenAbi from '../constants/RewardToken.json';
+import { Form } from 'web3uikit';
+import { ethers } from 'ethers';
+
+function StakeForm() {
+  const stakingAddress = "0x30b4b1150BCD5B838812a41BD9d46d61FF0c32c9"; //replace this with the address where you have deployed your staking Smart Contract
+  const tesTokenAddress = "0x1fa9C2FDdc7C747aeCbf7D4E6dAd280da4D5607b"; //replace this with the address where you have deployed your Reward Token Smart Contract
+
+  const { runContractFunction } = useWeb3Contract();
+
+  let approveOptions = {
+    abi: TokenAbi.abi,
+    contractAddress: tesTokenAddress,
+    functionName: 'approve'
+  };
+
+  let stakeOptions = {
+    abi: StakingAbi.abi,
+    contractAddress: stakingAddress,
+    functionName: 'stake'
+  };
+
+  async function handleStakeSubmit(data) {
+    const amountToApprove = data.data[0].inputResult;
+    approveOptions.params = {
+      amount: ethers.utils.parseEther(amountToApprove, 'ether'),
+      spender: stakingAddress
+    };
+
+    const tx = await runContractFunction({
+      params: approveOptions,
+      onError: (error) => console.log(error),
+      onSuccess: () => {
+        handleApproveSuccess(approveOptions.params.amount);
+      }
+    });
+  }
+
+  async function handleApproveSuccess(amountToStakeFormatted) {
+    stakeOptions.params = {
+      amount: amountToStakeFormatted
+    };
+    try {
+    const tx = await runContractFunction({
+      params: stakeOptions,
+      onError: (error) => console.log(error)
+    });
+
+    await tx.wait(0)
+    console.log('Stake transaction complete');
+
+  } catch (e) { console.log(e); }	
+  }
+
+  return (
+    <>
+    <div className='text-black'>
+   
+      <Form
+      
+        onSubmit={handleStakeSubmit}
+        data={[
+          {
+            inputWidth: '50%',
+            name: 'Amount to stake ',
+            type: 'number',
+            value: '',
+            key: 'amountToStake'
+          }
+        ]}
+        title="Stake Now!"
+      ></Form>
+     
+    
+    </div>
+   
+  </>
+  );
+}
+
+export default StakeForm;
